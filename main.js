8e9801b5da41f97060254177b9fdcc28ac92331b
Trever O <toveson9522@gmail.com>
2021-12-14 23:58:50 -0700

    added ability to save data when extension is closed

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
+// get the stored data and display it
+chrome.storage.sync.get({ tasks: "" }, (result) => {
+    document.getElementById("list_task").innerHTML = result.tasks;
+});
+
 // submitting it
-form = document.forms.myform;
+form = document.forms.myForm;
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  list_item = document.getElementById("list_task");
-  var list = document.createElement("li");
-  var listcontent = document.createTextNode(form.task.value);
-  list.appendChild(listcontent);
-  list_item.appendChild(list);
-  form.task.value = "";
+    event.preventDefault();
+    list_item = document.getElementById("list_task");
+    var list = document.createElement("li");
+    var listContent = document.createTextNode(form.task.value);
+    list.appendChild(listContent);
+    list_item.appendChild(list);
+    form.task.value = "";
+
+    // save the task
+    tasks = document.getElementById("list_task").innerHTML;
+    chrome.storage.sync.set({ tasks: tasks }, () => {
+        console.log(tasks);
+    });
 });
 
 // marking it as complete
 const ul = document.getElementById("list_task");
 ul.addEventListener("click", function (e) {
-  e.target.classList.toggle("checked");
+    e.target.classList.toggle("checked");
+
+    //save it
+    tasks = document.getElementById("list_task").innerHTML;
+    chrome.storage.sync.set({ tasks: tasks }, () => {
+        console.log(tasks);
+    });
 });
